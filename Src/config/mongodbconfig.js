71f20627c247adad13d36a7e0c3855fc854bb661
abhishek rawat <abhiryl420@gmail.com>
2025-05-07 19:24:22 +0530

    Creating Indexes

--- Src/config/mongodbconfig.js
+++ Src/config/mongodbconfig.js
@@ -1,19 +1,33 @@
 import { MongoClient } from "mongodb";
 let client;
-export const connectToDb = async()=>{
-    try{
-     const clientInstance = await MongoClient.connect(process.env.DB_URL);
-      client = clientInstance;
-      console.log("MongoDB connected successfully");
-    }
-    catch(err){
-    console.log("MongoDB didnot connected",err);
-    }
+export const connectToDb = async () => {
+  try {
+    const clientInstance = await MongoClient.connect(process.env.DB_URL);
+    client = clientInstance;
+    createIndexs(client.db())
+    console.log("MongoDB connected successfully");
+  }
+  catch (err) {
+    console.log("MongoDB didnot connected", err);
+  }
 }
 
-export  const getDb =()=>{
+// CreateIndexes
+const createIndexs = async (db) => {
+  try {
+    await db.collection("products").createIndex({_id : 1});
+    await db.collection("products").createIndex({price:1,category: 1});
+    console.log("Indexes Created Successfully")
+  }
+  catch (err) {
+    console.log(err);
+    
+  }
+}
+
+export const getDb = () => {
   return client.db();
 }
-export const transactionSession =()=>{
+export const transactionSession = () => {
   return client;
 }
